d2dee3227406fbb70efc00be686875b0c4d82fea
iretoms <iretomiwajonathan@gmail.com>
2022-05-29 19:43:14 -0700

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const answerEl = document.querySelector('.answer')
-const inputEl = document.querySelector('.input')
+const prevTextEl = document.querySelector('.prevText')
+const currentTextEl = document.querySelector('.currentText')
 const clearBtn = document.getElementById('clear')
 const backspaceBtn = document.getElementById('backspace')
 const equalsBtn = document.getElementById('equals')
@@ -10,19 +10,48 @@ const operatorButtons = document.querySelectorAll('.operator')
 
 numberButtons.forEach(numberButton =>{
     numberButton.addEventListener('click',()=>{
-        inputEl.innerText += numberButton.innerText    
+        appendNumber(numberButton.innerText)    
     })
 })
 
 operatorButtons.forEach(operatorButton =>{
     operatorButton.addEventListener('click',()=>{
-        inputEl.innerText += operatorButton.innerText    
+        chooseOperation(operatorButton.innerText)   
     })
 })
 
 clearBtn.addEventListener('click',()=>{
-    inputEl.innerText = ''
+    clear()
 })
+
+equalsBtn.addEventListener('click',()=>{
+    
+})
+
+
+
+
+function appendNumber(number){
+    if(number === '.' && currentTextEl.innerText.includes('.')) return
+    currentTextEl.innerText += number
+}
+
+function chooseOperation(operator){
+    currentTextEl.innerText += operator
+}
+
+function clear(){
+    currentTextEl.innerText = ''
+    prevTextEl.innerText = ''
+}
+
+function backspace(){
+
+}
+
+function compute(){
+    
+}
 
 
 
